76c486e07ad8ccc157609d03dec345c7ef178cbb
Cyroxin <Cyroxin@github.com>
2021-08-26 23:57:24 +0300

    HelloWorldMaterialDesign V2

--- Examples/HelloWorldMaterialDesign/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ Examples/HelloWorldMaterialDesign/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,21 +2,33 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.appcompat.app.ActionBar
+import android.view.Menu
+
+// Banner image source: https://cleanpublicdomain.com/
+// Uploader: https://cleanpublicdomain.com/vendor-dashboard/prashu8055/
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.setHomeAsUpIndicator(androidx.appcompat.R.drawable.abc_ic_ab_back_material);
+        actionBar?.setHomeButtonEnabled(true)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val title = findViewById<TextView>(R.id.textView)
         val btn = findViewById<FloatingActionButton>(R.id.send)
         val editbox = findViewById<EditText>(R.id.editbox)
-        btn.setOnClickListener { if(editbox.text.isEmpty()) { if (title.text == "Hello World") title.text = "Goodbye Summer!" else title.text = "Hello World"} else {title.text = "Hello " + editbox.text + '!'; editbox.text.clear()} }
+        btn.setOnClickListener { if(editbox.text.isEmpty()) { if (actionBar?.title == "Hello World") actionBar?.title = "Goodbye Summer!" else actionBar?.title = "Hello World"} else {actionBar?.title = "Hello " + editbox.text + '!'; editbox.text.clear()} }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.items, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
