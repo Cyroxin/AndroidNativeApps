f40898ef866617ea0642a77f82470f981cdc6e63
Cyroxin <Cyroxin@github.com>
2021-08-27 20:11:23 +0300

    HelloWorldMaterialDesign V3

--- Examples/HelloWorldMaterialDesign/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ Examples/HelloWorldMaterialDesign/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,30 +1,115 @@
 package com.example.helloworld
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import androidx.appcompat.app.ActionBar
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.android.material.snackbar.Snackbar
 
 // Banner image source: https://cleanpublicdomain.com/
 // Uploader: https://cleanpublicdomain.com/vendor-dashboard/prashu8055/
 
+const val CHANNEL_ID = "defaultchannel"
+
 class MainActivity : AppCompatActivity() {
+    var layout : ConstraintLayout? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createNotificationChannel()
 
         val actionBar: ActionBar? = supportActionBar
         actionBar?.setHomeAsUpIndicator(androidx.appcompat.R.drawable.abc_ic_ab_back_material);
         actionBar?.setHomeButtonEnabled(true)
         actionBar?.setDisplayHomeAsUpEnabled(true)
 
+        layout = findViewById<ConstraintLayout>(R.id.mainlayout)
+
         val btn = findViewById<FloatingActionButton>(R.id.send)
         val editbox = findViewById<EditText>(R.id.editbox)
-        btn.setOnClickListener { if(editbox.text.isEmpty()) { if (actionBar?.title == "Hello World") actionBar?.title = "Goodbye Summer!" else actionBar?.title = "Hello World"} else {actionBar?.title = "Hello " + editbox.text + '!'; editbox.text.clear()} }
+        var repeats : Int = 0 // If user is repeatedly doing the same action
+
+        btn.setOnClickListener {
+            if(editbox.text.isEmpty()) {
+                if (actionBar?.title == "Hello World") {
+                    actionBar?.title = "Goodbye Summer!"
+                    if(repeats > 2)
+                    {
+                        Toast.makeText(applicationContext, "Stop it", Toast.LENGTH_SHORT).show()
+                        repeats = 0
+                    }
+                }
+                else {
+                    actionBar?.title = "Hello World"
+                    repeats++
+
+                }
+            }
+            else {
+                actionBar?.title = "Hello " + editbox.text + '!'
+                editbox.text.clear()
+                repeats = 0
+            }
+        }
 
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+
+
+        if(item.title == "like")
+        {
+            // when you want to send the notification
+            val notif = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.likered)
+            .setContentTitle("Back at ya")
+            .setContentText("I like you too!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+            NotificationManagerCompat.from(this).notify(1, notif)
+        }
+        else
+            Snackbar.make(layout!!,"This does not do anything.", Snackbar.LENGTH_SHORT)
+                .setBackgroundTint(Color.WHITE)
+                .setTextColor(Color.BLACK)
+                .show()
+
+
+
+        return super.onOptionsItemSelected(item)
+
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+             val channel = NotificationChannel(CHANNEL_ID, CHANNEL_ID, NotificationManager.IMPORTANCE_DEFAULT)
+                 .apply {
+                description = getString(R.string.app_name)
+            }
+
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                 getSystemService(Context.NOTIFICATION_SERVICE) as
+                        NotificationManager
+             notificationManager.createNotificationChannel(channel)
+            }
+        }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
